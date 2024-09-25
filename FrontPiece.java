25df2a211826e41d3682f0cb192bfea0d993937b
alazhar98 <92651154+alazhar98@users.noreply.github.com>
2024-09-25 12:23:13 +0400

    Add files via upload

---
+++ FrontPiece.java
@@ -0,0 +1,40 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class FrontPiece {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter the number of elements in the first array:");
+        int sizeA = in.nextInt();
+        int[] array = new int[sizeA];
+        System.out.println("Enter the elements of the first array:");
+        for (int i = 0; i < sizeA; i++) {
+            array[i] = in.nextInt();
+        }
+
+        int[] resultA = FPiece(array);
+
+        System.out.println("Front piece of the array: " + Arrays.toString(resultA));
+
+        in.close();
+
+    }
+/*
+result = new array with maximum of two index only
+import the value for the new array by for loop for only 2 index
+
+ */
+    public static int[] FPiece(int[] nums) {
+
+        int[] result = new int[Math.min(nums.length, 2)];
+
+        for (int i = 0; i < result.length; i++) {
+            result[i] = nums[i];
+        }
+
+        return result;
+    }
+}
