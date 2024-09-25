a7c9ad406f5e813ec22be6e8166e87a2e32edf27
alazhar98 <92651154+alazhar98@users.noreply.github.com>
2024-09-25 12:05:59 +0400

    Add files via upload

---
+++ font11.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class font11 {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter the number of elements in the first array:");
+        int sizeA = in.nextInt();
+        int[] arrayA = new int[sizeA];
+        System.out.println("Enter the elements of the first array:");
+        for (int i = 0; i < sizeA; i++) {
+            arrayA[i] = in.nextInt();
+        }
+        System.out.println("Enter the number of elements in the Second array:");
+        int sizeB = in.nextInt();
+        int[] arrayB = new int[sizeB];
+        System.out.println("Enter the elements of the first array:");
+        for (int i = 0; i < sizeB; i++) {
+            arrayB[i] = in.nextInt();
+        }
+        // Arrays.toString : convert array to human readable
+        int[] result = front(arrayA, arrayB);
+        System.out.println("Resulting array: " + Arrays.toString(result));
+
+        in.close();
+
+    }
+/*
+first i initialize result as array with only 2 index one from first array and the other from second array
+then index =0
+i check if the arrays is greater than 0 then will give me the first index from both arrays
+ i return result and index from both arrays
+ */
+    public static int[] front(int[] arrayA, int[] arrayB){
+        int[] result = new int[2];
+        int index = 0;
+
+        if (arrayA.length > 0) {
+            result[index++] = arrayA[0];
+        }
+        if (arrayB.length > 0) {
+            result[index++] = arrayB[0];
+        }
+        return Arrays.copyOf(result, index);
+
+    }
+
+}
