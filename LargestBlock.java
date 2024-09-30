a49dbafe97253474172a064d3faab091f3fbf598
alazhar98 <92651154+alazhar98@users.noreply.github.com>
2024-09-30 11:53:52 +0400

    Add files via upload

---
+++ LargestBlock.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class LargestBlock {
+    public static void main(String[] args ){
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = in.nextLine();
+
+        int maxBlockLength = maxBlock(input);
+        System.out.println("The length of the largest block is: " + maxBlockLength);
+
+        in.close();
+    }
+    /*
+    i initialize maxLength and currentLength=1
+    i go through the string and if i found character that same as character before i increment current length
+    else i find the max between maxLength and current length
+    
+     */
+    public static int maxBlock(String str){
+        int maxLength=1;
+        int currentLength=1;
+
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) == str.charAt(i - 1)) {
+                currentLength++;
+
+            }
+            else {
+                maxLength=Math.max(maxLength,currentLength);
+                currentLength=1;
+            }
+
+
+        }
+        maxLength=Math.max(maxLength,currentLength);
+        return maxLength;
+        }
+}
+
