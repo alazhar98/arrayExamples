4de27b2f9a6dffb269c38356fbcafeb1ac72b5ca
alazhar98 <92651154+alazhar98@users.noreply.github.com>
2024-10-07 12:07:57 +0400

    Add files via upload

--- ImplementStack.java
+++ ImplementStack.java
@@ -1,18 +1,44 @@
 import java.util.Stack;
 
 public class ImplementStack {
+    private int[] stackArray;
+    private int top;
+    private int capacity;
+
     public static void main(String[] args){
-        Stack<Integer> numbers = new Stack<>();
+        ImplementStack numbers = new ImplementStack(4);
 
         numbers.push(10);
         numbers.push(20);
         numbers.push(30);
-        System.out.println("After push:"+numbers);
-        numbers.pop();
-        System.out.println("After pop:"+numbers);
-        numbers.peek();
-        System.out.println("After peek:"+numbers);
+        System.out.println("pop:"+numbers.pop());
+        System.out.println("peek:"+numbers.peek());
+
+        System.out.println("is Empty:"+numbers.isEmpty());
 
 
     }
+    public ImplementStack (int size){
+        stackArray=new int[size];
+        capacity=size;
+        top=-1; // now stack is at initial is empty
+    }
+    public void push(int item){
+        if(top == capacity-1){
+            System.out.println("overflow");
+            return;
+        }
+        stackArray[++top]=item;
+
+    }
+    public int pop(){
+        return stackArray[top--];
+    }
+    public int peek(){
+        return stackArray[top];
+    }
+    public boolean isEmpty(){
+        return top==-1;
+    }
+
 }
