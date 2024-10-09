94b3b4282f8e8c1b467cdc63eab6d2184e69d7b3
Alazhar Alsalmani <92651154+alazhar98@users.noreply.github.com>
2024-10-09 13:19:27 +0400

    Add files via upload

---
+++ FindMissingNumber.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+public class FindMissingNumber {
+    public static void main(String[] args){
+        ArrayList<Integer> inputList = new ArrayList<>();
+        inputList.add(3);
+        inputList.add(0);
+        inputList.add(1);
+
+        int missingNumber = missingNumber(inputList);
+        System.out.println("Missing number: " + missingNumber);
+    }
+    //n is Size of the ArrayList
+    // i create i Hashset
+    // add all numbers from 0 to n to the set
+    // remove elements found in the ArrayList from the set
+    // remaining element in the set is the missing number
+    public static int missingNumber(ArrayList<Integer> nums){
+        int n = nums.size();
+        HashSet<Integer> setOfnumber = new HashSet<>();
+        for (int i = 0; i <= n; i++) {
+            setOfnumber.add(i);
+        }
+        for (int num : nums) {
+            setOfnumber.remove(num);
+        }
+        return setOfnumber.iterator().next();
+
+
+    }
+}
