c7aa4f8a7d8036e00e294aed5dca82d7a454a202
Alazhar Alsalmani <92651154+alazhar98@users.noreply.github.com>
2024-10-09 13:07:07 +0400

    Add files via upload

--- CheckForDublicatesBySet.java
+++ CheckForDublicatesBySet.java
@@ -1,17 +1,25 @@
+import java.util.ArrayList;
 import java.util.HashSet;
 
 public class CheckForDublicatesBySet {
     public static void main(String[] args) {
-        int[] arr1 = {1, 2, 3, 1};
-        int[] arr2 = {1, 2, 3};
+        ArrayList<Integer> arr1 = new ArrayList<>();
+        arr1.add(1);
+        arr1.add(2);
+        arr1.add(3);
+        arr1.add(1);
         System.out.println(hasDuplicates(arr1));
+        ArrayList<Integer> arr2= new ArrayList<>();
+        arr2.add(1);
+        arr2.add(2);
+        arr2.add(3);
         System.out.println(hasDuplicates(arr2));
     }
     // I make HashSet Name it numbers
-    // For every number in the array  add it to the HashSet
+    // For every number in the array  add it to t,he HashSet
     // the add return false if the number is already present
     // if duplicates found return true else return false
-    public static boolean hasDuplicates(int[] array){
+    public static boolean hasDuplicates(ArrayList<Integer> array){
         HashSet<Integer> numbers= new HashSet<>();
 
         for (int num:array){
