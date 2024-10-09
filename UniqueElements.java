013cde6eaa1c08d725693e059dae3dfc3a517287
Alazhar Alsalmani <92651154+alazhar98@users.noreply.github.com>
2024-10-09 12:20:36 +0400

    Add files via upload

--- UniqueElements.java
+++ UniqueElements.java
@@ -1,18 +1,20 @@
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
-// I make new linkHashset i name it numbers
-// I add every number in the array into the linkHashset
+// I make arrayList i add the numbers on it
+// I add the arrayList into the set 
 // LinkHashset will not change the order for the set
 public class UniqueElements {
     public static void main(String[] args){
-        Set<Integer> numbers = new LinkedHashSet<>();
-        int[] arr ={1,2,3,2,1};
-        for(int num:arr){
-            numbers.add(num);
-
-        }
+        ArrayList<Integer> array = new ArrayList<>();
+        array.add(1);
+        array.add(2);
+        array.add(3);
+        array.add(2);
+        array.add(1);
+        Set<Integer> numbers = new LinkedHashSet<>(array);
         System.out.println("The unique Numbers in the array : "+numbers);
 
 
