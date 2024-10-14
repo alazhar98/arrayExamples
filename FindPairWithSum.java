42e0fb6f5c72ebe04a36d5e5c658c5e12afef5b4
Alazhar Alsalmani <92651154+alazhar98@users.noreply.github.com>
2024-10-14 11:46:44 +0400

    Add files via upload

--- FindPairWithSum.java
+++ FindPairWithSum.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.HashSet;
 
 public class FindPairWithSum {
-    public static void main(String[] args ){
+    public static void main(String[] args) {
         ArrayList<Integer> input = new ArrayList<>();
         input.add(1);
         input.add(4);
@@ -17,19 +17,17 @@ public class FindPairWithSum {
     }
     // Calculate the complement (target-num)
     // if found pair by check if the numbers contains the complements return true
-    
+
 
     public static boolean hasPairWithSum(ArrayList<Integer> nums, int target) {
-        HashSet<Integer> numbers = new HashSet<>();
-
-        for (int num : nums) {
-            int complement = target - num;
-            if (numbers.contains(complement)) {
-                return true;
+        // Check every pair of numbers
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums.get(i) + nums.get(j) == target) {
+                    return true;
+                }
             }
-            numbers.add(num);
         }
-
         return false;
     }
 }
