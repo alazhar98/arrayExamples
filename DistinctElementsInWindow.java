44a70d80bc46b3293f6e16450190bcc703160b07
Alazhar Alsalmani <92651154+alazhar98@users.noreply.github.com>
2024-10-17 11:49:24 +0400

    Add files via upload

--- DistinctElementsInWindow.java
+++ DistinctElementsInWindow.java
@@ -1,44 +1,42 @@
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 
 public class DistinctElementsInWindow {
     public static void main(String[] args) {
         int[] arr = {1, 2, 1, 3, 4, 2, 3};
         int k = 4;
-        ArrayList<Integer> result = countDistinctInWindow(arr, k);
+
+        List<Integer> result = countDistinctInSlidingWindow(arr, k);
         System.out.println("Output: " + result);
     }
+/*
+Return empty if window size is greater than array length or invalid
+Remove the element going out of the window
+Add the new element coming into the window
+Store the count of distinct elements
+ */
+    public static List<Integer> countDistinctInSlidingWindow(int[] arr, int k) {
+        List<Integer> result = new ArrayList<>();
+        if (arr.length < k || k <= 0) {
+            return result;
+        }
 
-    public static ArrayList<Integer> countDistinctInWindow(int[] arr, int k) {
-        ArrayList<Integer> result = new ArrayList<>();
-        int[] count = new int[10];
+        HashSet<Integer> set = new HashSet<>();
 
 
         for (int i = 0; i < k; i++) {
-            count[arr[i]]++;
+            set.add(arr[i]);
         }
+        result.add(set.size());
 
-        result.add(countDistinct(count));
 
         for (int i = k; i < arr.length; i++) {
-
-            count[arr[i]]++;
-
-            count[arr[i - k]]--;
-
-
-            result.add(countDistinct(count));
+            set.remove(arr[i - k]);
+            set.add(arr[i]);
+            result.add(set.size());
         }
 
         return result;
-    }
-
-    private static int countDistinct(int[] count) {
-        int distinctCount = 0;
-        for (int i = 1; i < count.length; i++) {
-            if (count[i] > 0) {
-                distinctCount++;
-            }
-        }
-        return distinctCount;
     }
 }
