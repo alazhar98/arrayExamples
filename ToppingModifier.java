0b7f5fa2bd4ccd1549bc8bb8ceb9749d1c9ed90f
Alazhar Alsalmani <92651154+alazhar98@users.noreply.github.com>
2024-10-21 12:16:47 +0400

    i have change class name to updateToppingsMap

--- ToppingModifier.java
+++ ToppingModifier.java
@@ -5,14 +5,14 @@ public class ToppingModifier {
     public static void main(String[] args){
         Map<String, String> map = new HashMap<>();
         map.put("ice cream", "peanuts");
-        modifyMap(map);
+        updateToppingsMap(map);
         System.out.println(map);
     }
-    public static void modifyMap(Map<String, String> map){
+    public static void updateToppingsMap(Map<String, String> map){
         if (map.containsKey("ice cream")){
             map.put("ice cream","cherry");
 
         }
-        map.put("bread ","butter");
+        map.put("bread","butter");
     }
 }
