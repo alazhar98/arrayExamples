9d95a051092c27cfe4afe9ee5299efe8c72bba4e
Alazhar Alsalmani <92651154+alazhar98@users.noreply.github.com>
2024-10-21 11:28:17 +0400

    Update and Remove using hash map

---
+++ MapShare.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapShare {
+    public static void main(String[] args){
+        Map<String, String> map = new HashMap<>();
+        map.put("a", "aaa");
+        map.put("b", "bbb");
+        map.put("c", "ccc");
+        modifyMap(map);
+        System.out.println(map);
+    }
+    /*
+    check if map contains a and map get a not equal to null
+    initialize value to a value
+    put b to value a (aaa)
+    remove c if there
+    
+     */
+    public static void modifyMap(Map<String, String> map){
+        if (map.containsKey("a") && map.get("a") != null) {
+            String value = map.get("a");
+            map.put("b",value);
+
+        }
+        map.remove("c");
+    }
+}
