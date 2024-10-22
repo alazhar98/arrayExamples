9ae8fde8d70783255ba37478fda6f334c5f505d7
alazhar <alazhar9814@gmail.com>
2024-10-22 11:33:15 +0400

    i have make explain for user in input massage

--- WordCounter.java
+++ WordCounter.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class WordCounter {
     public static void main(String[] args ){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter strings (type 'exit' to finish):");
+        System.out.println("Enter strings make spaces between characters:");
 
         String input = scanner.nextLine();
 
