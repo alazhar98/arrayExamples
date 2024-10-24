89e75f3fdd5391900973bb16e143871449801698
alazhar <alazhar9814@gmail.com>
2024-10-24 08:24:25 +0400

    delete some spaces

--- DuplicateFinder.java
+++ DuplicateFinder.java
@@ -10,11 +10,9 @@ public class DuplicateFinder {
         // two HashMap for positions and counts
         Map<Integer, List<Integer>> positions = new HashMap<>();
         Map<Integer, Integer> counts = new HashMap<>();
-
         //loop to find duplicates
         for (int i = 0; i < numbers.length; i++) {
             int num = Integer.parseInt(numbers[i]);
-
             // to track positions
             if (!positions.containsKey(num)) {
                 positions.put(num, new ArrayList<>());
@@ -24,22 +22,16 @@ public class DuplicateFinder {
             counts.put(num, counts.getOrDefault(num, 0) + 1);
         }
         int sum = 0;
-
         //display duplicates+ positions
         for (Map.Entry<Integer, List<Integer>> entry : positions.entrySet()) {
             int num = entry.getKey();
             List<Integer> posList = entry.getValue();
-
-
             // if its appears more than one time
             if (posList.size() > 1) {
                 System.out.println(num + ": Occurs " + posList.size() + " times at positions " + posList);
                 sum += num; // Add to the sum
-
             }
         }
         System.out.println("Sum " + sum);
-
-
     }
 }
