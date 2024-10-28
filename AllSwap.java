d3eb906590062326df2aa33f9c102f1d0b378360
alazhar <alazhar9814@gmail.com>
2024-10-28 09:25:29 +0400

    change from remove entry to clear

--- AllSwap.java
+++ AllSwap.java
@@ -16,24 +16,24 @@ public class AllSwap {
 
     }
     public static String[] swaps(String[] strings){
-        Map<Character,Integer> firstCharacter = new HashMap<>();
+        Map<Character,Integer> firstCharMap  = new HashMap<>();
 
         for (int i=0;i<strings.length;i++){
             String str = strings[i];
             char firstChar = str.charAt(0);
 
-            if (firstCharacter.containsKey(firstChar)){
-                int swapIndex  = firstCharacter.get(firstChar);
+            if (firstCharMap .containsKey(firstChar)){
+                int swapIndex  = firstCharMap .get(firstChar);
 
                 // swap
                 String temp = strings[i];
                 strings[i] = strings[swapIndex];
                 strings[swapIndex] = temp;
 
-                firstCharacter.remove(firstChar);
+                firstCharMap.clear();
             }
             else {
-                firstCharacter.put(firstChar,i);
+                firstCharMap .put(firstChar,i);
             }
         }
         return strings;
