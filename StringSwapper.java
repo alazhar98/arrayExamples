ec9d4e7a4d99aee0f1e95b3cfe0efed0e3b2317f
alazhar <alazhar9814@gmail.com>
2024-10-28 09:17:09 +0400

    i have change the name and the method name

---
+++ StringSwapper.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class StringSwapper {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(swapFirstCharStrings(new String[]{"ab", "ac"})));
+        System.out.println(Arrays.toString(swapFirstCharStrings(new String[]{"ax", "bx", "cx", "cy", "by", "ay", "aaa", "azz"})));
+
+    }
+
+    /*
+    i make hashMap
+    for loop for swapping
+    i check if the hashMap contain first character then ,
+    after the swap done i need to remove the first character from HashMap
+     
+     */
+    public static String[] swapFirstCharStrings(String[] strings) {
+        Map<Character, Integer> charIndex = new HashMap<>();
+        boolean[] swapped = new boolean[strings.length];
+
+        for (int i = 0; i < strings.length; i++) {
+            char firstCharacter = strings[i].charAt(0);
+
+            if (charIndex.containsKey(firstCharacter)) {
+                int swapIndex = charIndex.get(firstCharacter);
+
+                // Only swap if does not swap before
+                if (!swapped[swapIndex]) {
+                    String temp = strings[i];
+                    strings[i] = strings[swapIndex];
+                    strings[swapIndex] = temp;
+                    swapped[swapIndex] = true;
+                }
+            } else {
+                charIndex.put(firstCharacter, i);
+            }
+        }
+        return strings;
+    }
+}
